da51d0027b33560d2eab85899cbaf43cd2f324c5
Svarog916 <kau.ninja916@gmail.com>
2022-12-21 21:42:43 +0300

    Add empty-project

---
+++ src/test/java/find/itTeam/FinaItTeamApplicationTests.java
@@ -0,0 +1,13 @@
+package find.itTeam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinaItTeamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
