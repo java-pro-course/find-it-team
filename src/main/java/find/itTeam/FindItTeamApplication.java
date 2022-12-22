fe296500557ab5da1394a5bfe177ea712f86b405
Svarog916 <kau.ninja916@gmail.com>
2022-12-22 15:25:32 +0300

    Fixed a typo

--- src/main/java/find/itTeam/FindItTeamApplication.java
+++ src/main/java/find/itTeam/FindItTeamApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FinaItTeamApplication {
+public class FindItTeamApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FinaItTeamApplication.class, args);
+		SpringApplication.run(FindItTeamApplication.class, args);
 	}
 
 }
