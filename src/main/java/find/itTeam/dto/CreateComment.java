403cbeda8337f2c00816290c17c49b9f74686f6d
whyyougoinmyaccount <egorlomey2@gmail.com>
2023-01-19 18:10:53 +0300

    Egor lomey

--- src/main/java/find/itTeam/dto/CreateComment.java
+++ src/main/java/find/itTeam/dto/CreateComment.java
@@ -1,28 +1,12 @@
 package find.itTeam.dto;
 
-import java.time.LocalDate;
+import lombok.Data;
 
+import java.time.LocalDate;
+@Data
 public class CreateComment {
     private String text;
     private LocalDate dateTime;
-    public CreateComment(Long Id, String text, LocalDate dateTime){
-        this.text = text;
-        this.dateTime = dateTime;
-    }
 
-    public String getText() {
-        return text;
-    }
 
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalDate getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDate dateTime) {
-        this.dateTime = dateTime;
-    }
 }
