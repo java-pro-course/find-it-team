777ac67dca4f3fcbda0648662e0a83b86a70947c
Artem <artemtarbeevv@gmail.com>
2023-01-19 20:40:22 +0500

    121

--- src/main/java/find/itTeam/dto/ChangeComment.java
+++ src/main/java/find/itTeam/dto/ChangeComment.java
@@ -1,28 +1,11 @@
 package find.itTeam.dto;
 
-import java.time.LocalDate;
 
+import lombok.Data;
+
+import java.time.LocalDate;
+@Data
 public class ChangeComment {
     private String text;
     private LocalDate dateTime;
-    public ChangeComment(String text, LocalDate dateTime){
-        this.text = text;
-        this.dateTime = dateTime;
-    }
-
-    public String getText() {
-        return text;
-    }
-
-    public void setText(String text) {
-        this.text = text;
-    }
-
-    public LocalDate getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDate dateTime) {
-        this.dateTime = dateTime;
-    }
 }
--- src/main/java/find/itTeam/dto/CreateNewPost.java
+++ src/main/java/find/itTeam/dto/CreateNewPost.java
@@ -1,30 +1,12 @@
 package find.itTeam.dto;
 
-import java.time.LocalDate;
+import lombok.Data;
 
+import java.time.LocalDate;
+@Data
 public class CreateNewPost {
 
     private String content;
     private LocalDate dateTime;
 
-    public CreateNewPost(String content, LocalDate dateTime){
-        this.content = content;
-        this.dateTime = dateTime;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public LocalDate getDateTime() {
-        return dateTime;
-    }
-
-    public void setDateTime(LocalDate dateTime) {
-        this.dateTime = dateTime;
-    }
 }
