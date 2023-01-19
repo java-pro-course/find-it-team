61a59a7a30dcea958f8e846cbfc9dee9d6d86976
whyyougoinmyaccount <egorlomey2@gmail.com>
2023-01-19 18:15:34 +0300

    Egor Lomey

--- src/main/java/find/itTeam/dto/CreateNewUser.java
+++ src/main/java/find/itTeam/dto/CreateNewUser.java
@@ -1,47 +1,12 @@
 package find.itTeam.dto;
 
-public class CreateUser {
+import lombok.Data;
+
+@Data
+public class CreateNewUser {
     private String name;
     private String surname;
     private String email;
     private String password;
 
-    public CreateUser(String name, String surname, String email, String password){
-        this.name = name;
-        this.surname = surname;
-        this.email = email;
-        this.password = password;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getSurname() {
-        return surname;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
 }
