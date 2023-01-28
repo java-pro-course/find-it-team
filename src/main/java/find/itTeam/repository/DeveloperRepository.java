883ac444a55cbce17e1d1088dbacd8e25950743c
Artem <artemtarbeevv@gmail.com>
2023-01-28 15:45:02 +0500

    контроллер

--- src/main/java/find/itTeam/repository/DeveloperRepository.java
+++ src/main/java/find/itTeam/repository/DeveloperRepository.java
@@ -4,7 +4,6 @@ import find.itTeam.entity.DeveloperEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
 
 @Repository
 public interface DeveloperRepository extends JpaRepository<DeveloperEntity, Long> {
